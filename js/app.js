225fbf85c770930970df2c4838e2cb8219913134
amcravila <amcravila@gmail.com>
2018-08-01 21:09:22 -0300

    Fotos restaurantes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-// Add your JavaScript
+function myMap() {
+  var local = {lat: -23.5576413, lng: -46.6623001};
+  var map = new google.maps.Map(
+      document.getElementById('googleMap'), {zoom: 19, center: local});
+  var marker = new google.maps.Marker({position: local, map: map});
+}
+
+$(document).ready(function() {
+
+for (var v of restaurantes) {
+  var newI = $("<img>");
+  newI.addClass("img");
+  newI.attr("src", v.image);
+  $(".images").append(newI);
+}
+
+
+});
