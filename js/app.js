011e362a7cd6248121a7e24326711f4a459fde55
amcravila <amcravila@gmail.com>
2018-08-04 09:52:10 -0300

    Filtro restaurantes

--- js/app.js
+++ js/app.js
@@ -7,17 +7,65 @@ function myMap() {
 
 $(document).ready(function() {
 
-setTimeout(function() {
-  $('#splashscreen').delay(2000);
-  $('#main').show();
-}, 3000);
+//TELA INICIAL
+  setTimeout(function() {
+    $('#splashscreen').fadeOut(2000);
+    $('#main').delay(2000).fadeIn('slow');
+  }, 2000);
 
-for (var v of restaurantes) {
-  var newI = $("<img>");
-  newI.addClass("img");
-  newI.attr("src", v.image);
-  $(".images").append(newI);
-}
+//FOTOS RESTAURANTES
+  $.each(restaurantes, function(index, value) {
+    var newI = $('<img>');
+    newI.addClass('img');
+    newI.attr('src', value.image);
+    $('.images').append(newI);
+  });
+
+//FILTRO RESTAURANTES
+  $('#filter').click(function () {
+    var inputValue = $('#restaurant-type').val().toLowerCase();
+
+    $.each(restaurantes, function(index, value) {
+      if (value.type !== inputValue) {
+        $('.images').fadeOut();
+      }
+    });
+
+
+    var filterResult = restaurantes.filter(function(value) {
+      if (inputValue === value.type) {
+        return value;
+      }
+    });
+
+    var returnImg = filterResult.map(function(value) {
+    return value.image;
+    });
+    console.log(returnImg);
+
+    $.each(returnImg, function(index, value) {
+      console.log(value);
+      var newI = $('<img>');
+      newI.addClass('img');
+      newI.attr('src', value);
+      console.log(newI);
+      $('.imagesFilter').append(newI);
+      $('.imagesFilter').fadeIn();
+    });
+  });
+
+
+
+
+
+  $('#restaurant-type').on('input', function () {
+    if($(this).val() === '') {
+      for (var v of restaurantes) {
+        $('.imagesFilter').fadeOut();
+        $('.images').fadeIn('slow');
+      };
+    }
+  })
 
 
 });
